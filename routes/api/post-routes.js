4a2c75c6452b6ce70d181ae760a630fd5df28b34
Mouse0226 <mouse1987.ml@gmail.com>
2022-03-19 11:09:28 -0500

    Added comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
+const { sequelize } = require('../../models/User');
 
 // get all users
 router.get('/', (req, res) => {
@@ -10,10 +11,18 @@ router.get('/', (req, res) => {
           'post_url', 
           'title', 
           'created_at',
-          [sequelize.literal('(SELECT COUT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -40,6 +49,14 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -25,6 +25,14 @@ router.get('/:id', (req, res) => {
             {
               model: Post,
               attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
               model: Post,
@@ -67,7 +75,7 @@ router.post('/login', (req, res) => {
     User.findOne({
         where: {
             email: req.body.email
-        }
+        },
     }).then(dbUserData => {
         if (!dbUserData) {
             res.status(400).json({ message: 'No user with that email address!' });
